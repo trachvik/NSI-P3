06e8e5e898888c4156d0c8bd474920ce6f5ce8e3
trachvik <vitrachta@gmail.com>
2026-05-24 13:59:17 +0200

    Topics subscription

--- src/main_rpi.py
+++ src/main_rpi.py
@@ -7,16 +7,33 @@ import dht
 from umqtt.robust import MQTTClient
 from secrets_local import SSID, PASSWORD, MQTT_BROKER, LOGIN
 
+toggle_counter = 0
+
 TOPIC_PUB_TELEMETRY = f"cvut/nsi/2026/{LOGIN}/telemetry".encode('utf-8')
+TOPIC_SUB_LED = f"cvut/nsi/2026/{LOGIN}/led".encode('utf-8')
+TOPIC_SUB_ALL_TELEMETRY = b"cvut/nsi/2026/+/telemetry"
+
 
 sensor = dht.DHT22(machine.Pin(20))
 led = machine.Pin("LED", machine.Pin.OUT)
+led.value(0)
+
+warning_timer = machine.Timer()
 
 publish_requested = False
 
 def publish_isr(t):
     global publish_requested
     publish_requested = True
+
+def warning_isr(t):
+    global toggle_counter
+    led.toggle()
+    toggle_counter += 1
+    if toggle_counter < 6:
+        warning_timer.init(period=100, mode=machine.Timer.ONE_SHOT, callback=warning_isr)
+    else:
+        toggle_counter = 0
 
 
 def connect_wifi(timeout_s=15):
@@ -35,6 +52,27 @@ def iso8601_utc_now():
     return "{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.{:03d}Z".format(
         y, mo, d, h, mi, s, ms
     )
+
+def mqtt_callback(topic, msg):
+    topic_str = topic.decode('utf-8')
+    msg_str = msg.decode('utf-8').strip().upper()
+    
+    if topic_str == TOPIC_SUB_LED.decode('utf-8'):
+        if msg_str == "ON":  led.value(1); print("Příchozí příkaz: ZAPNOUT")
+        elif msg_str == "OFF": led.value(0); print("Příchozí příkaz: VYPNOUT")
+        elif msg_str == "TOGGLE": led.toggle(); print("Příchozí příkaz: TOGGLE")
+
+    elif topic_str.endswith("/telemetry"):
+        sender_login = topic_str.split('/')[3]
+        if sender_login != LOGIN:
+            try:
+                data = json.loads(msg.decode("utf-8"))
+                temperature = data.get("temperature")
+                if isinstance(temperature, (int, float)) and temperature > 30:
+                    warning_timer.init(period=100, mode=machine.Timer.ONE_SHOT, callback=warning_isr)
+            except ValueError:
+                print("Neplatny JSON")
+
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -55,8 +93,10 @@ publish_timer.init(period=10000, mode=machine.Timer.PERIODIC, callback=publish_i
 
 client_id = f"rpi_pico_{LOGIN}_{machine.unique_id().hex()}"
 client = MQTTClient(client_id=client_id, server=MQTT_BROKER)
-#client.set_callback(mqtt_callback)
+client.set_callback(mqtt_callback)
 client.connect()
+client.subscribe(TOPIC_SUB_LED)
+client.subscribe(TOPIC_SUB_ALL_TELEMETRY)
 
 
 while True:
